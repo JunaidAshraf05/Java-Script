c004dc71361c6cc36055ab3650aba83ec0724f28
sStudent <junaidashraf2500@gmail.com>
2024-09-27 18:05:50 +0530

    JavaScript

--- lec 2/index.js
+++ lec 2/index.js
@@ -1,4 +1,30 @@
-var age = 25;
- if(true){
-    console.log(age);
- }
+// var age = 25;
+//  if(true){
+//     console.log(age);
+//  }
+
+ const accountId = 123455 
+  /*  cont and let use krna hai var nahi*/
+let accountEmail = "hdjhahdya"
+var accountPasssword = "234566" 
+accountCity = "Bilaspur"
+
+// accountId = 2 not allowed
+
+
+accountEmail = 'jhweiwq'
+accountPasssword= "kjiheiljiwej"
+accountCity = "bwjhejwehw"
+
+console.table([accountId , accountEmail , accountPasssword ,accountCity])
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
