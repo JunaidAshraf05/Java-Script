191c94a8cf4094b7a3f251b3ca05d809a9031b18
sStudent <junaidashraf2500@gmail.com>
2024-09-27 18:33:32 +0530

    javascript

--- Datatype/index.js
+++ Datatype/index.js
@@ -4,3 +4,26 @@
 // alert( 3 + 3) //  we are using node js not browser
 
 console.log(3+3); // code redablitiy should be high
+
+let name = "juniad"
+let age = 12
+let isloogedIn = "false"
+let state = null 
+
+// number => 2 to  power 53
+// bigint
+// string => ""
+//boolean =.  true and false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+
+
+// OBECT
+
+console.log(typeof age);
+console.log(typeof null);
+//null ek object 
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
