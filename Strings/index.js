e860499ebe67ffb697fbdc64642203319d6a8d90
sStudent <junaidashraf2500@gmail.com>
2024-09-28 23:58:26 +0530

    JavaScript

---
+++ Strings/index.js
@@ -0,0 +1,38 @@
+const name = "junaid"
+const repocount = 50 
+
+console.log(name + repocount + "Value"); /* Old method to print */
+
+//put value in curlybracket to define
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`) /*new way to print*/ 
+
+// another way to declare strings
+ const gameName = new String('juniadhc')
+console.log(gameName[0]);
+
+//to get the value of length
+console.log(gameName.length);
+
+//to get the value in uppercase
+console.log(gameName.toUpperCase());
+
+//to get charcater in indexes
+console.log(gameName.charAt(2));
+
+//to get the value of index of charcter
+console.log(gameName.indexOf('u'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8 , 4)
+console.log(newString);
+
+//trim wll delete the space in string
+const newStringOne = '     junaid        '
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+//replace
+const url = "https://junaid.com/junaidashraf"
+console.log(url.replace('%20','__'))
