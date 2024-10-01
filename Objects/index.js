8dc7babd4d5870b1c4f6b21dee2c0a469c10d067
sStudent <junaidashraf2500@gmail.com>
2024-10-01 13:15:57 +0530

    JavaScript

--- Objects/index.js
+++ Objects/index.js
@@ -1,8 +1,50 @@
 // singleton 
 // objects literals
 
-const Jsuser = {
-    name :"Junaid"
-}
+const mySym = Symbol("key1")
 
-myArray =["h" , "i"]
+const Jsuser = {
+    name :"Junaid",
+
+    "Full name":"junaid ashraf",
+
+    [mySym]: "mykey1",
+//to acess in symbol put my sym in []
+    age:19,
+    location:'Bilaspur',
+    email:  "junaid@google.com",
+    isloggedIn: false,
+    lastLoginDays:["Monday" , "saturday"]
+
+}
+//to acess the data 
+console.log(Jsuser.age)
+
+console.log(Jsuser["age"])
+
+//to acess the full name statement we can only use that sqaure bracket 
+console.log(Jsuser["Full name"])
+
+console.log(Jsuser[mySym])
+
+
+console.log(typeof Jsuser.mySym)
+
+
+//to update the values just text the new value
+
+Jsuser.email = "junaid@chatgpt.com"
+
+//to freeze the values to not to change
+Object.freeze(Jsuser)
+
+//to check the values are frozen or not
+console.log(Object.isFrozen(Jsuser))
+ 
+console.log(Jsuser);
+
+//to add function 
+Jsuser.greeting = function(){
+    console.log("hello world");
+}
+console.log(Jsuser.greeting);
