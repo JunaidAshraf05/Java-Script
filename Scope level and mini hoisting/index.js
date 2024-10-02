93226df44fdf8fee82b33f44f50029f9bf6fa6dc
sStudent <junaidashraf2500@gmail.com>
2024-10-02 12:56:58 +0530

    JavaScript

---
+++ Scope level and mini hoisting/index.js
@@ -0,0 +1,21 @@
+function one(){
+    const username = "Junaid"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+one()
+
+
+if(true){
+    const username = "junaid"
+    if(username === "junaid"){
+    const website = " youtube"
+console.log(username + website );}
+    }
