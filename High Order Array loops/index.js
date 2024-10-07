5994e97b9f8a0e1872ee01e4b482e3d41aa1959a
sStudent <junaidashraf2500@gmail.com>
2024-10-07 16:43:14 +0530

    JavaScript

--- High Order Array loops/index.js
+++ High Order Array loops/index.js
@@ -4,6 +4,28 @@
 
 const arr = [ 1 ,2,3,4,5]
 
-for (const element of object) {
+for (const num of arr) {
+    console.log(num);
+}
+ const greetings = "hello world!"
+ 
+ for(const greet of greetings){
+    console.log(`Each char is ${greet}`)
+ }
+
+
+ const map = new Map()
+ map.set('IN', 'India')
+ map.set('USA' , 'United state of america')
+console.log(map);
+
+for( const key of map){
+    console.log(key);
+
+}
+
+
+for( const[key , value ] of map){
+    console.log(key, '-:,value');
     
 }
