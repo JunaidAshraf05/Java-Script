958a593d40f789de38445e4163cb747329d7ecf5
sStudent <junaidashraf2500@gmail.com>
2024-10-07 17:30:25 +0530

    JavaScript

---
+++ High Order Array loops/index2.js
@@ -0,0 +1,21 @@
+const myObject = {
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:"seift by apple"
+}
+for(const key in myObject){
+    console.log(`${key} shortcut is for${myObject[key]}`);
+}
+
+const programming = ["js" , "rb" , "py" ,"java", "cpp"]
+
+for( const key in programming){
+    console.log(key);
+}
+for( const key in programming){
+    console.log(programming);
+}
+for( const key in programming){
+    console.log(programming[key]);
+}
