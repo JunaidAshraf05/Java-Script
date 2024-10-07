64826b48d526f63c67d32e46fce13c0b401338a2
sStudent <junaidashraf2500@gmail.com>
2024-10-07 16:11:50 +0530

    JavaScript

---
+++ High Order Array loops/index.js
@@ -0,0 +1,9 @@
+// for of
+//["" , " " , ""]
+//[{} , {}, {}]
+
+const arr = [ 1 ,2,3,4,5]
+
+for (const element of object) {
+    
+}
---
+++ This function Arrow/index.js
@@ -0,0 +1,18 @@
+const user = {
+    usrname:"junaid",
+    Price:500,
+
+    welcomeMessage:function() {
+        console.log(`$(this.username), welcome to website`);
+    }
+}
+
+user.welcomeMessage()
+user.username="sam"
+user.welcomeMessage()
+
+
+const chai = function({
+    let username = "junaid"
+    console.log
+})
